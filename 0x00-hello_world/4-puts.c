71bd2609aec666cbc7afafe1a5ba36f6b9084952
Abdelrahman_Ahmed <abdoahlawy704@gmail.com>
2023-03-17 20:17:21 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main - Learing how to print with puts
+*puts - show a sentence
+*retuen : 0 (success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
