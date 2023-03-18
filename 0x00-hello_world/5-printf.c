839c1ddad77db7e18b86f672e3faad4285c92004
Abdelrahman_Ahmed <abdoahlawy704@gmail.com>
2023-03-17 20:36:01 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*main - the main page
+*return: always 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
