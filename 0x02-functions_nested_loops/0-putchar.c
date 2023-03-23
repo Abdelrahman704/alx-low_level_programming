b39859bf78d393ed03e5e565ce2ee2cd04b0f30a
Abdelrahman_Ahmed <abdoahlawy704@gmail.com>
2023-03-23 16:35:16 -0700

    first task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#inclode "main.h"
+/**
+ * main - Determine the last digit of a random number.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
