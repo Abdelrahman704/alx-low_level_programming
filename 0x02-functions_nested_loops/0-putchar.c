ad2afeaa6db5c887567f824fb3022908145e12f5
Abdelrahman_Ahmed <abdoahlawy704@gmail.com>
2023-03-23 17:10:05 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,11 +7,14 @@
  */
 int main(void)
 {
-	char AR[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ch++)
-		putchar(AR[ch]);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
