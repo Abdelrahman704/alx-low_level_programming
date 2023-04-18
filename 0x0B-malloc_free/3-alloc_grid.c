64ac9bf7bf87bad00d25aa8c2aa17f3cce2f9f00
Abdelrahman704 <abdoahlawy704@gmail.com>
2023-04-18 05:32:16 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 {
 	int **ptr, a, b;
 
-	ptr = malloc(sizeof(int) * height);
+	ptr = malloc(sizeof(*tab) * height);
 	if (width <= 0 || ptr == 0 || height <= 0)
 	{
 		return (NULL);
