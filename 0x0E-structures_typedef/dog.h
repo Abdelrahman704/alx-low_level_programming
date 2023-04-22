1b2805f24293795b0c496aae35118af675275777
Abdelrahman704 <abdoahlawy704@gmail.com>
2023-04-22 06:18:07 -0700

    dogs

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - class of dogs
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: build a struct of dogs
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
