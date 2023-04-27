ad29ebd2e82bc2efd162984d88e4e4a2da4db515
Abdelrahman704 <abdoahlawy704@gmail.com>
2023-04-27 13:36:23 -0700

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * int_index - knowing the index
+ * @array: an array
+ * @size: the size of array
+ * @cmp: pointer to a function
+ *
+ * Return: the integer index
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && size && cmp)
+	{
+		for (i = 0; i < size - 1; i++)
+		{
+			if (cmp(array[i])
+				return (i);
+		}
+	}
+	return (-1);
+}
